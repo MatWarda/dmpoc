333807a8683931e8db7c81d99185a0393574e34a
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-18 17:06:00 +0100

    Branch004

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'DM003.sqlite3',
+        'NAME': BASE_DIR / 'DM004.sqlite3',
     }
 }
 
