e4e5a292ae40ad7a7cddd294f4270372762e7e1e
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-18 16:11:59 +0100

    test2

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'DM1.sqlite3',
+        'NAME': BASE_DIR / 'DM2.sqlite3',
     }
 }
 
