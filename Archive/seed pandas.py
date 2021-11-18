8e298b59c00582f40aba087a1127e20972c03fb3
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-18 17:54:08 +0100

    Archived old

--- Archive/seed pandas.py
+++ Archive/seed pandas.py
@@ -1,10 +1,10 @@
 import csv, sqlite3
 
-con = sqlite3.connect("./DataMart_db3.sqlite3") # change to 'sqlite:///your_filename.db'
+con = sqlite3.connect("DataMart_db3.sqlite3") # change to 'sqlite:///your_filename.db'
 cur = con.cursor()
 #cur.execute("CREATE TABLE t (col1, col2);") # use your column names here
 
-with open('dataset.csv', 'r') as fin: # `with` statement available in 2.5+
+with open('../dataset.csv', 'r') as fin: # `with` statement available in 2.5+
     # csv.DictReader uses first line in file for column headings by default
     dr = csv.DictReader(fin) # comma is default delimiter
     to_db = [(i['id'], i['first_name'], i['last_name'], i['instrument'], i['singer']) for i in dr]
