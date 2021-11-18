0ae5878d26974b45d01167e8a6eedfa384c87362
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-18 15:47:48 +0100

    Feed initial commit

---
+++ seed.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import sqlite3
+
+
+con = sqlite3.connect("./DM1.sqlite3")
+cols = ['id', 'instrument', 'singer']
+df = pd.read_csv('dataset.csv', sep=";", usecols=cols, nrows=15)
+#df = pd.read_csv('dataset.csv', sep=";")
+df.to_sql('DMmigration_musician', con, if_exists='replace', index=False, chunksize=15)
+#con.execute("SELECT * FROM DMmigration_musician").fetchall()
+con.close()
