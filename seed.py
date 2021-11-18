3dba23876e0a791b4325d994bfb4b370dd3ddd3d
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-18 17:01:16 +0100

    Nix

--- seed.py
+++ seed.py
@@ -3,9 +3,9 @@ import sqlite3
 
 
 con = sqlite3.connect("./DM003.sqlite3")
-cols = ['id', 'instrument', 'singer']
-df = pd.read_csv('dataset.csv', sep=";", usecols=cols, nrows=15)
-#df = pd.read_csv('dataset.csv', sep=";")
+#cols = ['id', 'instrument', 'singer']
+#df = pd.read_csv('dataset.csv', sep=";", usecols=cols, nrows=15)
+df = pd.read_csv('dataset.csv', sep=";", nrows=15)
 df.to_sql('DMmigration_musician', con, if_exists='replace', index=False, chunksize=15)
 #con.execute("SELECT * FROM DMmigration_musician").fetchall()
 con.close()
