6378b921183354b53b774fc82f752216c62979e8
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-23 00:24:23 +0100

    Configfile.ini working

--- seed.py
+++ seed.py
@@ -8,19 +8,19 @@ config.read("configfile.ini")
 datamart_name = config.get('data_mart', 'name')
 datamart_type = config.get('data_mart', 'output_type')
 datamart_file = (datamart_name + '.' + datamart_type)
+source_table = config.get('source', 'table')
 
 column_names = config.get('data_mart', 'model_manual')
 cols = column_names.split()
 
 con = sqlite3.connect("./Northwind_large.sqlite")
-df = pd.read_sql_query("SELECT * FROM OrderDetail", con)
-df_red = df[cols] #check if change in configfile has an effect
-con.close()
+df = pd.read_sql_query("SELECT * from " + source_table, con)
+df_red = df[cols]
 
 
 cxn = sqlite3.connect(datamart_file)
-df_red.to_sql('OrderDetail', cxn, if_exists='replace', index=False, chunksize=15)
-SQLdf = pd.read_sql_query("SELECT * from OrderDetail", cxn)
+df_red.to_sql(source_table, cxn, if_exists='replace', index=False, chunksize=15)
+SQLdf = pd.read_sql_query("SELECT * from " + source_table, cxn)
 print('Data Mart table:')
 print(SQLdf.head())
 cxn.close()
