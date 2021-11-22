f72306193316ef426f5c6c477d832d56d824a68e
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-22 18:25:19 +0100

    merge to master

--- seed.py
+++ seed.py
@@ -2,10 +2,12 @@ import pandas as pd
 import sqlite3
 
 
-con = sqlite3.connect("./DM2.sqlite3")
-cols = ['id', 'instrument', 'singer']
+con = sqlite3.connect("./DM4.sqlite3")
+cols = ['OrderId', 'ProductId', 'UnitPrice', 'Quantity']
 df = pd.read_csv('dataset.csv', sep=";", usecols=cols, nrows=15)
 #df = pd.read_csv('dataset.csv', sep=";")
-df.to_sql('DMmigration_musician', con, if_exists='replace', index=False, chunksize=15)
-#con.execute("SELECT * FROM DMmigration_musician").fetchall()
+df.to_sql('OrderDetail', con, if_exists='replace', index=False, chunksize=15)
+SQLdf = pd.read_sql_query("SELECT * from OrderDetail", con)
+print('Created Data Mart table:')
+print(SQLdf.head())
 con.close()
