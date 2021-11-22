379243b8183d894f0a6976a39a4ea549e180c40a
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-22 23:11:17 +0100

    Included making migration

--- DMmigration/models.py
+++ DMmigration/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class OrderDetail(models.Model):
-    OrderId = models.CharField(max_length=50)
-    ProductId = models.CharField(max_length=50)
+    OrderId = models.CharField(max_length=100)
+    ProductId = models.CharField(max_length=100)
     UnitPrice = models.CharField(max_length=100)
     Quantity = models.CharField(max_length=100)
--- seed.py
+++ seed.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import sqlite3
 
-con = sqlite3.connect("./DM4.sqlite3")
+con = sqlite3.connect("./Northwind_large.sqlite")
+df = pd.read_sql_query("SELECT * FROM OrderDetail", con)
 cols = ['OrderId', 'ProductId', 'UnitPrice', 'Quantity']
-df = pd.read_csv('dataset.csv', sep=";", usecols=cols, nrows=15)
-#df = pd.read_csv('dataset.csv', sep=";")
-df.to_sql('OrderDetail', con, if_exists='replace', index=False, chunksize=15)
-SQLdf = pd.read_sql_query("SELECT * from OrderDetail", con)
+df_red = df[cols]
+con.close()
+
+
+cxn = sqlite3.connect("./DataMart.sqlite3")
+df_red.to_sql('OrderDetail', cxn, if_exists='replace', index=False, chunksize=15)
+SQLdf = pd.read_sql_query("SELECT * from OrderDetail", cxn)
 print('Data Mart table:')
 print(SQLdf.head())
-con.close()
+cxn.close()
