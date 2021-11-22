a1ff4a152f2d7c6ce568535e8d7ce95e2a471f4b
Mateusz Warda <mateusz_warda@mckinsey.com>
2021-11-22 23:11:17 +0100

    Connecting SQLite

--- seed.py
+++ seed.py
@@ -1,11 +1,12 @@
 import pandas as pd
 import sqlite3
 
-con = sqlite3.connect("./DM4.sqlite3")
+con = sqlite3.connect("./Northwind_large.sqlite")
 cols = ['OrderId', 'ProductId', 'UnitPrice', 'Quantity']
-df = pd.read_csv('dataset.csv', sep=";", usecols=cols, nrows=15)
-#df = pd.read_csv('dataset.csv', sep=";")
-df.to_sql('OrderDetail', con, if_exists='replace', index=False, chunksize=15)
+df = pd.read_sql_query("SELECT * FROM OrderDetail", con)
+df_red = df[cols]
+
+df_red.to_sql('OrderDetail', con, if_exists='replace', index=False, chunksize=15)
 SQLdf = pd.read_sql_query("SELECT * from OrderDetail", con)
 print('Data Mart table:')
 print(SQLdf.head())
